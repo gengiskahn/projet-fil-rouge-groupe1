602cf855ca6dc35f0fe78b21f668aa9e8a74e613
John Doe <johndoe@example.com>
2022-05-06 00:36:26 +0200

    Created vagrant files

--- VAGRANT/install_minikube.sh
+++ VAGRANT/install_minikube.sh
@@ -1,25 +1,55 @@
 #!/bin/sh
-yum -y update
-yum -y install epel-release
-yum -y install git libvirt qemu-kvm virt-install virt-top libguestfs-tools bridge-utils
-yum install socat -y
-yum install -y conntrack
+
+# update system
+cd /etc/yum.repos.d/
+sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
+sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
+yum update -y
+yum -y install epel-release 
+yum install -y sshpass
+yum -y install wget
+yum install -y git
+yum upgrade -y
+
+# allow ssh password
+sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+systemctl restart sshd
+
+# install docker
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
 usermod -aG docker vagrant
+
+# Start Docker
+systemctl enable docker
 systemctl start docker
-yum -y install wget
-wget https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
-chmod +x minikube-linux-amd64
-mv minikube-linux-amd64 /usr/bin/minikube
-curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
-chmod +x kubectl
-mv kubectl  /usr/bin/
-echo '1' > /proc/sys/net/bridge/bridge-nf-call-iptables
-systemctl enable docker.service
-su - vagrant -c "minikube start --kubernetes-version=1.20.0 --driver=none"
+
+# add user jenkins
+useradd jenkins
+usermod -aG docker jenkins
+usermod -aG wheel jenkins
+sudo echo "jenkins        ALL=(ALL)       NOPASSWD: ALL" > /etc/sudoers.d/jenkins
+su jenkins -c "ssh-keygen -q -f ~/.ssh/id_rsa -N ''"
+
+# Enable local dns on each server
+echo -e "192.168.100.10 jenkins" >> /etc/hosts
+echo -e "192.168.100.11 staging" >> /etc/hosts
+echo -e "192.168.100.12 production" >> /etc/hosts
+
+# Install Minikube
+sudo cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOF
+sudo yum install -y kubectl
+
 yum install bash-completion -y
 echo 'source <(kubectl completion bash)' >> ~vagrant/.bashrc
 echo 'alias k=kubectl' >> ~vagrant/.bashrc
 echo 'complete -F __start_kubectl k' >> ~vagrant/.bashrc
-echo "For this Stack, you will use $(ip -f inet addr show enp0s8 | sed -En -e 's/.*inet ([0-9.]+).*/\1/p') IP Address"
+echo "For this Stack, you will use $(ip -f inet addr show eth1 | sed -En -e 's/.*inet ([0-9.]+).*/\1/p') IP Address"
