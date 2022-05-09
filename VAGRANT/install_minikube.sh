f93bd92f03453513fb8960a9bf83d645ffe262e5
catherine Joly <catherine@cjoly.fr>
2022-05-09 10:52:41 +0200

    fix infra on minikube

--- VAGRANT/install_minikube.sh
+++ VAGRANT/install_minikube.sh
@@ -1,55 +1,42 @@
 #!/bin/sh
-
-# update system
-cd /etc/yum.repos.d/
-sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
-sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
-yum update -y
-yum -y install epel-release 
-yum install -y sshpass
-yum -y install wget
-yum install -y git
-yum upgrade -y
-
-# allow ssh password
-sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
-systemctl restart sshd
-
-# install docker
-curl -fsSL https://get.docker.com -o get-docker.sh
-sh get-docker.sh
-usermod -aG docker vagrant
-
-# Start Docker
-systemctl enable docker
+sudo yum -y update
+sudo yum -y install epel-release
+sudo yum -y install libvirt qemu-kvm virt-install virt-top libguestfs-tools bridge-utils
+yum install socat -y
+sudo yum install -y conntrack
+sudo curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+sudo usermod -aG docker vagrant
 systemctl start docker
-
-# add user jenkins
-useradd jenkins
-usermod -aG docker jenkins
-usermod -aG wheel jenkins
-sudo echo "jenkins        ALL=(ALL)       NOPASSWD: ALL" > /etc/sudoers.d/jenkins
-su jenkins -c "ssh-keygen -q -f ~/.ssh/id_rsa -N ''"
-
-# Enable local dns on each server
-echo -e "192.168.100.10 jenkins" >> /etc/hosts
-echo -e "192.168.100.11 staging" >> /etc/hosts
-echo -e "192.168.100.12 production" >> /etc/hosts
-
-# Install Minikube
-sudo cat <<EOF > /etc/yum.repos.d/kubernetes.repo
-[kubernetes]
-name=Kubernetes
-baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
-enabled=1
-gpgcheck=1
-repo_gpgcheck=1
-gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
-EOF
-sudo yum install -y kubectl
-
-yum install bash-completion -y
-echo 'source <(kubectl completion bash)' >> ~vagrant/.bashrc
-echo 'alias k=kubectl' >> ~vagrant/.bashrc
-echo 'complete -F __start_kubectl k' >> ~vagrant/.bashrc
-echo "For this Stack, you will use $(ip -f inet addr show eth1 | sed -En -e 's/.*inet ([0-9.]+).*/\1/p') IP Address"
+sudo yum -y install wget
+sudo wget https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo chmod +x minikube-linux-amd64
+sudo mv minikube-linux-amd64 /usr/bin/minikube
+sudo curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+sudo chmod +x kubectl
+sudo mv kubectl  /usr/bin/
+sudo echo '1' > /proc/sys/net/bridge/bridge-nf-call-iptables
+sudo systemctl enable docker.service
+echo "For this Stack, you will use $(ip -f inet addr show enp0s8 | sed -En -e 's/.*inet ([0-9.]+).*/\1/p') IP Address"
+echo "Connect through ssh on the VM and then run 'minikube start --driver=none' please don't run the command as root user or using any sudo command, use vagrant user"
+#!/bin/sh
+sudo yum -y update
+sudo yum -y install epel-release
+sudo yum -y install libvirt qemu-kvm virt-install virt-top libguestfs-tools bridge-utils
+yum install socat -y
+sudo yum install -y conntrack
+sudo curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+sudo usermod -aG docker vagrant
+systemctl start docker
+sudo yum -y install wget
+sudo wget https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo chmod +x minikube-linux-amd64
+sudo mv minikube-linux-amd64 /usr/bin/minikube
+sudo curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+sudo chmod +x kubectl
+sudo mv kubectl  /usr/bin/
+sudo echo '1' > /proc/sys/net/bridge/bridge-nf-call-iptables
+sudo systemctl enable docker.service
+echo "For this Stack, you will use $(ip -f inet addr show enp0s8 | sed -En -e 's/.*inet ([0-9.]+).*/\1/p') IP Address"
+echo "Connect through ssh on the VM and then run 'minikube start --driver=none' please don't run the command as root user or using any sudo command, use vagrant user"
